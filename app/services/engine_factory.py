d1bbaed6e6f7860301628e348b9a978321a2b0dc
小河那旁 <13097208215@163.com>
2026-06-08 11:48:28 +0800

    feat(engine_factory):数据库同步引擎工厂类,根据用户请求的数据库类型返回对应的同步引擎实例

---
+++ app/services/engine_factory.py
@@ -0,0 +1,23 @@
+# -- coding: utf-8 --
+# @Author: 胡H
+# @File:  app/services/engine_factory.py
+# @Created: 2026/6/8 9:58
+# @LastModified: 
+# Copyright (c) 2026 by 胡H, All Rights Reserved.
+# @desc: 数据库同步引擎工厂类,根据用户请求的数据库类型返回对应的同步引擎实例
+
+from app.schemas.tsync import DBSyncReq
+from app.services.sync_service import DatabaseSyncEngine
+from app.services.mongo_sync_engine import MongoSyncEngine
+
+
+class EngineFactory:
+    @staticmethod
+    def create(req: DBSyncReq):
+        db_type = req.db_type.lower()
+        if db_type == "mongodb":
+            return MongoSyncEngine(req)
+        elif db_type in ("mysql", "postgresql"):
+            return DatabaseSyncEngine(req)
+        else:
+            raise ValueError(f"不支持的数据库类型: {req.db_type}")
