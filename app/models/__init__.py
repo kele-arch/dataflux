216cda7df68081ebfe9ea46fe07d9916f7a1fac0
小河那旁 <13097208215@163.com>
2026-07-27 11:35:57 +0800

    feat():补全orm模型导入

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -2,6 +2,13 @@
 # @Author: 胡H
 # @File: app/models/__init__.py
 # @Created: 2025/11/19 10:37
-# @LastModified: 
+# @LastModified:
 # Copyright (c) 2025 by 胡H, All Rights Reserved.
 # @desc: ORM 模型
+
+from app.models.bashModel import BaseModelMixin, generate_uuid
+from app.models.collectRecordModel import CollectRecord
+from app.models.collectTaskModel import CollectTask
+from app.models.dataSourceModel import DataSource
+from app.models.otherModel import SysLog
+from app.models.taskLogModel import TaskLog, SyncExecutionLog, FtpFileRecord, OssFileRecord
